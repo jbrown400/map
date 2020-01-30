8feb82e644f29a646de9d585082f771248942cb4
jbrown400 <justin.brown400@gmail.com>
2020-01-29 21:05:17 -0600

    Setting up basic window and a button that prints to the console.

---
+++ map.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter.ttk import *
+import tkinter.font as font
+import RPi.GPIO as GPIO
+
+
+
+# Create the main window
+window = Tk()
+
+# Specify the font family, size, and weight
+myFont = font.Font(family = 'Helvetica', size = 36, weight = 'normal')
+# Set window title that will be shown in the title bar
+window.title("Where has Justin been?")
+# Set window size
+window.geometry('800x400')
+
+def close():
+    GPIO.cleanup()
+    window.destroy()
+
+def test():
+    print("Testing button")
+    
+    
+btn = Button(window, text="Test", font = myFont, command = test, height = 2, width = 6)
+btn.pack(side = BOTTOM)
+
+# Properly shut down the program if the user uses the "X" button on the window
+window.protocol("WM_DELETE_WINDOW", close)
+
+
+# Ke
