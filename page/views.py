05eac4a49a1d8d754857d37d7b399c3a771601a8
jbrown400 <justin.brown400@gmail.com>
2020-01-30 20:57:27 -0600

    Displaying a basic list of the countries in the DB and the value of the visit property.

--- page/views.py
+++ page/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Continent, Country
 
 
 def index(request):
-	return HttpResponse("Hello")
+	continents = Continent.objects.all()
+	countries = Country.objects.all()
+
+	context = {
+		'continents': continents,
+		'countries': countries
+	}
+	return render(request, 'page/index.html', context)
