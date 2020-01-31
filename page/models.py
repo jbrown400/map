ba24a035aaca48dacff37efa5fa3978f37c7f900
jbrown400 <justin.brown400@gmail.com>
2020-01-30 20:12:45 -0600

    Updated models.

--- page/models.py
+++ page/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 
+class Continent(models.Model):
+	# Continent name
+	name = models.CharField(max_length=15)
+
+	def __str__(self):
+		return self.name
+
+
 class Country(models.Model):
+	# The continent this country belongs to
+	continent = models.ForeignKey(Continent, on_delete=models.CASCADE, null=True)
 	# Country name
 	name = models.CharField(max_length=30)
 	# Boolean for whether or not I've been to that country
