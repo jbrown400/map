9102725e9a58c4161b3b001d67377c92ab1db97f
jbrown400 <justin.brown400@gmail.com>
2020-02-03 00:36:43 -0600

    I hate how much I care about styling.

--- page/static/page/script.js
+++ page/static/page/script.js
@@ -54,54 +54,36 @@ $('#full_screen_btn').click(function() {
 
 function showCountries(id) {
 	var all = document.getElementsByClassName("country_item")
+	var items;
 	for (var i = 0; i < all.length; i++) {
 		all[i].style["display"] = "none";
 	}
 	switch (id) {
 		case 1: // North America
-			var na = document.getElementsByClassName("continent_1")
-			na.background
-			for (var i = 0; i < na.length; i++) {
-				na[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_1")
 			break;
 		case 2: // South America
-			var sa = document.getElementsByClassName("continent_2")
-			for (var i = 0; i < sa.length; i++) {
-				sa[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_2")
 			break;
 		case 3: // Europe
-			var eu = document.getElementsByClassName("continent_3")
-			for (var i = 0; i < eu.length; i++) {
-				eu[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_3")
 			break;
 		case 4: // Africa
-			var af = document.getElementsByClassName("continent_4")
-			for (var i = 0; i < af.length; i++) {
-				af[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_4")
 			break;
 		case 5: // Asia
-			var as = document.getElementsByClassName("continent_5")
-			for (var i = 0; i < as.length; i++) {
-				as[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_5")
 			break;
 		case 6: // Australia and Oceania
-			var au = document.getElementsByClassName("continent_6")
-			for (var i = 0; i < au.length; i++) {
-				au[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_6")
 			break;
 		case 7: // Antarctica
-			var an = document.getElementsByClassName("continent_7")
-			for (var i = 0; i < an.length; i++) {
-				an[i].style["display"] = "flex";
-			}
+			items = document.getElementsByClassName("continent_7")
 			break;
 		default: // Default to NA
 			break;
+	}
+	for (var i = 0; i < items.length; i++) {
+		items[i].style["display"] = "flex";
 	}
 }
