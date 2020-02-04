69bc57a1a8e1c0d94985bf97bc6b72362d7bff62
jbrown400 <justin.brown400@gmail.com>
2020-02-04 11:25:00 -0600

    Moving fullscreen button and making it invisible since it doesn't provide functionality related to the project.

--- page/views.py
+++ page/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.core import serializers
 from .models import Continent, Country
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 dataPin = 11
 latchPin = 13
@@ -11,7 +10,7 @@ clockPin = 15
 
 # Populate the main page
 def index(request):
-	setup()
+	# setup()
 	sum = 0
 
 	# GET request code
@@ -22,7 +21,7 @@ def index(request):
 		if request.POST['action'] == "toggle":
 			if request.POST['toggle'] == "0":
 				print("Woo!")
-				single(0)
+				# single(0)
 				return JsonResponse({})
 		print(request.POST.getlist('visited'))
 		# Update the entries in the DB
@@ -39,7 +38,7 @@ def index(request):
 				sum += pow(2, (cou.id - 1))
 
 		# Send value to shift registers (Can I just import RPi.GPIO in the django project?
-		single(sum)
+		# single(sum)
 
 		# Just return empty
 		response_data = {}
@@ -51,7 +50,7 @@ def index(request):
 		if (cou.visited is True):
 			sum += pow(2, (cou.id - 1))
 
-	single(sum)
+	# single(sum)
 
 	context = {
 		'continents': continents,
@@ -59,35 +58,33 @@ def index(request):
 	}
 	return render(request, 'page/base.html', context)
 
-def setup():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(dataPin, GPIO.OUT)
-	GPIO.setup(latchPin, GPIO.OUT)
-	GPIO.setup(clockPin, GPIO.OUT)
-
-def single(val):
-	print(val)
-	GPIO.output(latchPin, GPIO.LOW)
-	shift(val)
-	GPIO.output(latchPin, GPIO.HIGH)
-
-def shift(val):
-	# Convert value to binary string and cut off the '0b' in the beginning
-	binary_string = bin(val)[2:]
-	print('Binary String: ' + binary_string)
-
-	# Add leading zeros
-	l = len(binary_string)
-	for j in range(0, (16-l)):
-		binary_string = "0" + binary_string
-
-	print('Binary String: ' + binary_string)
-
-	for i in range(0, len(binary_string)):
-
-		GPIO.output(clockPin, GPIO.LOW)
-		GPIO.output(dataPin, int(binary_string[i]) and GPIO.HIGH or GPIO.LOW)
-
-		GPIO.output(clockPin, GPIO.HIGH)
-
-
+# def setup():
+# 	GPIO.setmode(GPIO.BOARD)
+# 	GPIO.setup(dataPin, GPIO.OUT)
+# 	GPIO.setup(latchPin, GPIO.OUT)
+# 	GPIO.setup(clockPin, GPIO.OUT)
+#
+# def single(val):
+# 	print(val)
+# 	GPIO.output(latchPin, GPIO.LOW)
+# 	shift(val)
+# 	GPIO.output(latchPin, GPIO.HIGH)
+#
+# def shift(val):
+# 	# Convert value to binary string and cut off the '0b' in the beginning
+# 	binary_string = bin(val)[2:]
+# 	print('Binary String: ' + binary_string)
+#
+# 	# Add leading zeros
+# 	l = len(binary_string)
+# 	for j in range(0, (16-l)):
+# 		binary_string = "0" + binary_string
+#
+# 	print('Binary String: ' + binary_string)
+#
+# 	for i in range(0, len(binary_string)):
+#
+# 		GPIO.output(clockPin, GPIO.LOW)
+# 		GPIO.output(dataPin, int(binary_string[i]) and GPIO.HIGH or GPIO.LOW)
+#
+# 		GPIO.output(clockPin, GPIO.HIGH)
